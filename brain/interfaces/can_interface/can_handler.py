d43f6793e72d45867466a96111c497581758504c
Florimond Lalande <38161801+Flowpy@users.noreply.github.com>
2021-02-06 17:07:35 +0100

    Rewrite du module can_handler: parsing entiers négatifs, meilleur logging et corrections diverses.

--- brain/interfaces/can_interface/can_handler.py
+++ brain/interfaces/can_interface/can_handler.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 
-from brain.interfaces.interface_description.python_parser import Frame, devicesList, messagesList, framesList
+from brain.interfaces.interface_description.python_parser import Frame, devicesById, devicesByName, framesByCmdId
 
 from threading import Thread, Lock
 from typing import Dict
 import logging
+import struct
 import time
-# import traceback
 
 # python-can library
 import can
 can.rc['interface'] = 'socketcan_ctypes'
 
+can_logger = logging.getLogger('can_handler')
+
+msg_count = 0
+
 
 class CanHandler:
 
-	def __init__(self, dev='can0', callback=None, autostart=True):
+	def __init__(self, dev='can1', callback=None, autostart=True):
 		self.lock = Lock()
 
 		# Callback format: def func_name(frame_type: Frame, fields: Dict)
@@ -42,61 +46,74 @@ class CanHandler:
 					self.on_can_message(message)
 				except Exception as e:
 					# traceback.print_exc()
-					logging.warning('Received invalid can frame:')
-					logging.warning(message)
-					logging.warning('with following exception:')
-					logging.warning(e)
+					can_logger.warning('Received invalid can frame:')
+					can_logger.warning(message)
+					can_logger.warning('with following exception:')
+					can_logger.warning(e)
 
 		except Exception as e:
 			self.bus.shutdown()
-			logging.error('Can reception interrupted with following exception:')
-			logging.error(e)
+			can_logger.error('Can reception interrupted with following exception:')
+			can_logger.error(e)
 
 	def on_can_message(self, message: can.Message):
 		'''
 			Parse messages from can and call Callback
 		'''
+		global msg_count
+
+		# Hopefully this avoid concurrency access races (spoiler: no xO)
+		msg_num = (msg_count := msg_count + 1)
+
+		# Get destination Device
+		dst_device = devicesById.get(message.arbitration_id)
+		if dst_device is None:
+			can_logger.warning(f'{msg_num}: Received frame destinated to an unknown device id: {message.arbitration_id}')
+			can_logger.debug(f'{msg_num}: ' + self.frame_to_str(message))
+			return
+
 		if len(message.data) == 0:
-			logging.debug('Received empty frame, ignoring.')
+			can_logger.warning(f'{msg_num}: Received empty frame (dst={dst_device.name}), ignoring.')
+			can_logger.debug(f'{msg_num}: ' + self.frame_to_str(message))
 			return
 
 		# Get frame type
-		frame_type = None
-		for frame in framesList:
-			if message.data[0] == frame.cmd_id:
-				frame_type = frame
-				break
-		else:		# If no-break
-			logging.debug(f'Received unhandled frame command id: {message.data[0]}')
+		frame_type = framesByCmdId.get(message.data[0])
+		if frame_type is None:
+			can_logger.warning(f'{msg_num}: Received unhandled frame command id: {message.data[0]}')
+			can_logger.debug(f'{msg_num}: ' + self.frame_to_str(message))
 			return
 
-		# Get destination Device
-		device = None
-		for dev in devicesList:
-			if message.arbitration_id == dev.arbitration_id:
-				device = dev
-				break
-		else:		# If no-break
-			logging.debug(f'Received frame destinated to an unknown device id: {message.arbitration_id}')
-			return
-
-		logging.info(f'Received can frame "{frame_type.name}" to device: {device.name}')
+		if dst_device.name == 'all' or dst_device.name == 'bbb':
+			can_logger.info(f'{msg_num}: Received can frame "{frame_type.name}" for device: {dst_device.name}')
+		else:
+			can_logger.debug(f'{msg_num}: Received can frame "{frame_type.name}" for device: {dst_device.name}')
+		can_logger.debug(f'{msg_num}: ' + self.frame_to_str(message))
 
 		# Create message
 		try:
-			raw_data = frame.data[1:]
-			data_fields = {}
-			for field in frame_type.fields:
-				if field._type == 'byte':
-					data_fields[field.name] = raw_data[0]
-					raw_data = raw_data[1:]
-				else:
-					# Big-Endian, so MSByte first
-					data_fields[field.name] = (raw_data[0] << 8) | raw_data[1]
-					raw_data = raw_data[2:]
-		except IndexError:
-			logging.debug(f'Received incomplete frame of type {frame_type.name}, ignoring')
+			fmt = '>' + ''.join(('B' if field._type == 'byte' else 'h') for field in frame_type.fields)
+			data_fields = {
+				field.name: data for data, field in zip(struct.unpack_from(fmt, bytes(message.data), offset=1), frame_type.fields)
+			}
+		except struct.error:
+			can_logger.warning(f'{msg_num}: Received incomplete frame of type "{frame_type.name}" (dst={dst_device.name}), ignoring')
 			return
+
+		# try:
+		# 	raw_data = message.data[1:]
+		# 	data_fields = {}
+		# 	for field in frame_type.fields:
+		# 		if field._type == 'byte':
+		# 			data_fields[field.name] = raw_data[0]
+		# 			raw_data = raw_data[1:]
+		# 		else:
+		# 			# Big-Endian, so MSByte first
+		# 			data_fields[field.name] = (raw_data[0] << 8) | raw_data[1]
+		# 			raw_data = raw_data[2:]
+		# except IndexError:
+		# 	can_logger.warning(f'{msg_num}: Received incomplete frame of type "{frame_type.name}" (dst={dst_device.name}), ignoring')
+		# 	return
 
 		if self.callback is not None:
 			self.callback(frame_type, data_fields)
@@ -106,23 +123,41 @@ class CanHandler:
 			Handle message from ROS and build a frame from it
 		'''
 		with self.lock:
-			logging.info(f'New frame to send of type {frame_type.name}')
+			global msg_count
+
+			# Hopefully this avoid concurrency access races (spoiler: no xO)
+			msg_num = (msg_count := msg_count + 1)
+
+			can_logger.info(f'{msg_num}: New frame to send of type {frame_type.name}')
 
 			# Prepare data array
 			try:
-				data = []
-				for field in frame_type.fields:
-					if field._type != 'byte':
-						# Big-Endian, so MSByte first
-						data.append((data_fields[field.name] >> 8) & 0xff)
-					data.append(data_fields[field.name] & 0xff)
-			except KeyError:
-				logging.error('Message NOT sent: Malformed data_fields')
+				fmt = '>' + ''.join(('B' if field._type == 'byte' else 'h') for field in frame_type.fields)
+				data = [frame_type.cmd_id] + list(struct.pack(fmt, *(data_fields[field.name] for field in frame_type.fields)))
+			except (KeyError, struct.error):
+				can_logger.error(f'{msg_num}: Message NOT sent: {frame_type.name}: Malformed data_fields')
+				return
+
+			# try:
+			# 	data = [frame_type.cmd_id]
+			# 	for field in frame_type.fields:
+			# 		if field._type != 'byte':
+			# 			# Big-Endian, so MSByte first
+			# 			data.append((data_fields[field.name] >> 8) & 0xff)
+			# 		data.append(data_fields[field.name] & 0xff)
+			# except KeyError:
+			# 	can_logger.error(f'{msg_num}: Message NOT sent: {frame_type.name}: Malformed data_fields')
+			# 	return
+
+			dst_device = devicesByName.get(frame_type.destination)
+			if dst_device is None:
+				can_logger.error(f'{msg_num}: Message NOT sent: {frame_type.name}: Unknown destination')
+				return
 
 			# Setup output frame
 			frame = can.Message(
 				data=data,
-				arbitration_id=self.devices.by_name[frame_type.dest],
+				arbitration_id=dst_device.arbitration_id,
 				# is_remote_frame=False,
 				# is_error_frame=False,
 				is_extended_id=False,
@@ -132,7 +167,11 @@ class CanHandler:
 			# Send frame
 			try:
 				self.bus.send(frame)
-				logging.info(f'Message sent on {self.channel_info}:')
-				logging.info(frame)
+				# can_logger.debug(f'{msg_num}: Message sent on {self.bus.channel_info}:')
+				# can_logger.debug(f'{msg_num}: ' + self.frame_to_str(message))
 			except can.CanError as e:
-				logging.error(f'Message NOT sent: CanError: {e}')
+				can_logger.error(f'{msg_num}: Message NOT sent: {frame_type.name}: CanError: {e}')
+				return
+
+	def frame_to_str(self, message: can.Message):
+		return f'{message.channel}    {message.arbitration_id:03}   [{message.dlc}]   ' + ' '.join(f'{b:02x}' for b in message.data)
